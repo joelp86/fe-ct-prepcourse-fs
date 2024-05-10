d87159b84f42a557014bb4c1e1e11b11a938a48d
joelp86 <chulepereyra14@gmail.com>
2024-05-09 21:23:38 -0300

    mi primer commit

--- C5 Fundamentos Javascript/ejercicios/01.js
+++ C5 Fundamentos Javascript/ejercicios/01.js
@@ -1,6 +1,8 @@
 // Crea una variable de tipo string.
 // Reemplaza el valor de null por el correspondiente.
 
-const nuevoString = null;
+//const nuevoString = null;
+const nuevoString = "hola";
 
 module.exports = nuevoString;
+
--- C5 Fundamentos Javascript/ejercicios/08.js
+++ C5 Fundamentos Javascript/ejercicios/08.js
@@ -3,6 +3,8 @@ function esNumeroEntero(numero) {
   // Verifica si este es un número entero o no.
   // Returna true si lo es, de lo contrario, retorna false.
   // Tu código:
+  
 }
+
 
 module.exports = esNumeroEntero;
